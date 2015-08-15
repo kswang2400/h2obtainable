e2654ba67fab21f9ef39801059eac4ca679222f2
Conan Tzou <conantzou@gmail.com>
2015-08-15 13:23:33 -0700

    re setup backbone hook

--- app/assets/javascripts/water_hack.js
+++ app/assets/javascripts/water_hack.js
@@ -5,21 +5,12 @@ window.WaterHack = {
   Routers: {},
   initialize: function() {
     var $rootEl = $("#main");
-    var datum = new WaterHack.Collections.Datum();
-
-    var router = new CuteOrBoot.Routers.Router({
-      $rootEl: $rootEl,
-      users: users
-    });
-
-    var navbar = new CuteOrBoot.Views.Navbar({
-      router: router
-    });
-
-    $("div.cuteorboot").prepend(navbar.$el);
-    navbar.render();
 
     console.log("hi kevin");
     Backbone.history.start();
   }
 };
+
+$(document).ready(function(){
+  WaterHack.initialize();
+});
