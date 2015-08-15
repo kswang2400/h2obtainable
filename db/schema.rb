a41070728d1394905165828d515854e6f5733ed1
Kevin <kevinwang2400@gmail.com>
2015-08-15 14:26:28 -0700

    rename all our shit

--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,15 @@ ActiveRecord::Schema.define(version: 20150815185126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "data", force: :cascade do |t|
-    t.integer  "account_id",  null: false
+  create_table "usages", force: :cascade do |t|
     t.integer  "consumption", null: false
-    t.integer  "month",       null: false
+    t.string   "account_id",  null: false
+    t.string   "month",       null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "data", ["account_id"], name: "index_data_on_account_id", using: :btree
-  add_index "data", ["month"], name: "index_data_on_month", using: :btree
+  add_index "usages", ["account_id"], name: "index_usages_on_account_id", using: :btree
+  add_index "usages", ["month"], name: "index_usages_on_month", using: :btree
 
 end
