59c48b24d961f93226cd54723f4ccac3d3ee7c8e
Kevin <kevinwang2400@gmail.com>
2015-08-15 15:24:04 -0700

    api call for unique account ids

--- app/controllers/api/usages_controller.rb
+++ app/controllers/api/usages_controller.rb
@@ -6,5 +6,17 @@ module Api
 
       render "index.json.jbuilder"
     end
+
+    def show
+      @data = Usage.where(account_id: params[:id])
+
+      render "show.json.jbuilder"
+    end
+
+    def account_ids
+      @account_ids = Usage.select(:account_id).distinct
+
+      render json: @account_ids
+    end
   end
 end
