2894f5b54195b693c4785dc08dfd3e48114fd102
Kevin <kevinwang2400@gmail.com>
2015-08-15 13:19:19 -0700

    clean up

--- app/assets/javascripts/water_hack.js
+++ app/assets/javascripts/water_hack.js
@@ -4,22 +4,26 @@ window.WaterHack = {
   Views: {},
   Routers: {},
   initialize: function() {
-    var $rootEl = $("#main");
-    var datum = new WaterHack.Collections.Datum();
+    // var $rootEl = $("#main");
+    // var data = new WaterHack.Collections.Datum();
 
-    var router = new CuteOrBoot.Routers.Router({
-      $rootEl: $rootEl,
-      users: users
-    });
+    // var router = new CuteOrBoot.Routers.Router({
+    //   $rootEl: $rootEl,
+    //   users: users
+    // });
 
-    var navbar = new CuteOrBoot.Views.Navbar({
-      router: router
-    });
+    // var navbar = new CuteOrBoot.Views.Navbar({
+    //   router: router
+    // });
 
-    $("div.cuteorboot").prepend(navbar.$el);
-    navbar.render();
+    // $("div.cuteorboot").prepend(navbar.$el);
+    // navbar.render();
 
     console.log("hi kevin");
     Backbone.history.start();
   }
 };
+
+$(document).ready(function(){
+  WaterHack.initialize();
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
 
-  resources :data
+  namespace :api, defaults: { format: :json } do
+    resources :data, only: [:index]
+  end
 end
