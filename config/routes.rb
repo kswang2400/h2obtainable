9e37ae9ab2878cafd776b4e93c4ca53659ae7fca
Kevin <kevinwang2400@gmail.com>
2015-08-16 11:04:52 -0700

    dev api page done, needs more styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   get "developers", to: "static_pages#developers"
 
   namespace :api, defaults: { format: :json } do
-    resources :usages,          only: [:index]
-    resources :hourly_usages,   only: [:index]
+    resources :usages,          only: [:index, :show]
     resources :products,        only: [:index]
 
     get "list_account_ids", to: "usages#account_ids"
     get "list_all_usages", to: "usages#all"
   end
 end
+
+
+# 54f962a1-bd97-593d-b502-2079c72d922b
